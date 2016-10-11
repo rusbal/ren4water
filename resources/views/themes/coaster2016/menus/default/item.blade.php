35de77778be57175b0125d8cd63785abb25da18b
Raymond Usbal <raymond@philippinedev.com>
2016-10-11 18:29:22 +0800

    apply orange button join

--- resources/views/themes/coaster2016/menus/default/item.blade.php
+++ resources/views/themes/coaster2016/menus/default/item.blade.php
@@ -1,3 +1,3 @@
 <li class="{{ ($item->active)?' active':'' }}">
-    <a href="{{ $item->url }}">{{ $item->name }}</a>
+    <a href="{{ $item->url }}" class="{{ ($item->url == '/join')?'btn-no-margin btn-primary orange':'' }}">{{ $item->name }}</a>
 </li>
