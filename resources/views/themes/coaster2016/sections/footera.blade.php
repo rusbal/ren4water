3468b445d94ee3650173f92e885e9f8df8b595c6
Raymond Usbal <raymond@philippinedev.com>
2016-10-13 15:22:46 +0800

    footer with sub-menu

--- resources/views/themes/coaster2016/sections/footera.blade.php
+++ resources/views/themes/coaster2016/sections/footera.blade.php
@@ -1,16 +1,53 @@
 
 <footer>
     <div class="container">
-        <div class="row">
-            <div class="col-sm-12 text-center">
+        <div class="row text-center">
+            <div class="col-sm-3">
 
                 <!-- footer menu -->
                 <div class="footer-menu" style="">
-                  <ul class="nav navbar-nav">
-                    <li><a href="/supporters">Supporters</a> </li>
-                    <li><a href="/news">News</a> </li>
-                    <li><a href="/contact">Contact</a> </li>
+                  <h5>Supporters</h5>
+                  <ul>
+                    <li><a href="/supporters/financial">Our Financial Sponsors</a> </li>
+                    <li><a href="/supporters/suppliers">Our Strategic Suppliers</a> </li>
+                    <li><a href="/supporters/partners">Our Field Partners</a> </li>
+                  </ul>
+                </div>
+
+            </div>
+            <div class="col-sm-3">
+
+                <!-- footer menu -->
+                <div class="footer-menu" style="">
+                  <h5>News</h5>
+                  <ul>
+                    <li><a href="/news/water">Water News</a> </li>
+                    <li><a href="/blog">Project Blog</a> </li>
+                    <li><a href="/news/field">News From Field</a> </li>
+                  </ul>
+                </div>
+
+            </div>
+            <div class="col-sm-3">
+
+                <!-- footer menu -->
+                <div class="footer-menu" style="">
+                  <h5>Contact</h5>
+                  <ul>
+                    <li><a href="/contact">Contact Form</a> </li>
+                    <li><a href="/newsletter">Newsletter</a> </li>
+                  </ul>
+                </div>
+
+            </div>
+            <div class="col-sm-3">
+
+                <!-- footer menu -->
+                <div class="footer-menu" style="">
+                  <h5>Donate</h5>
+                  <ul>
                     <li><a class="btn-primary orange" href="/donate">Donate</a> </li>
+                    <li><a href="#">Sponsor</a> </li>
                   </ul>
                 </div>
 
